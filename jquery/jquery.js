fec2f176f6bac3e27681459ddcec415f0927cae6
Jose <antares069@gmail.com>
2020-01-16 20:17:42 -0600

    selectores_jQuery

--- jquery/jquery.js
+++ jquery/jquery.js
@@ -1,1 +1,12 @@
-alert("Hola mundo");
+$(document).ready(function(){
+
+    $("#rojo").css("background","red")
+              .css("color","white");  
+
+    $(".punteado").css("border","2px dashed black"); 
+    
+    $(".sin_borde").click(function(){
+        console.log("Click dado");
+        $(this).addClass("punteado");
+    });
+});
