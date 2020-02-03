c02cf1122dce9d3ec34dfefc41b23691575bf8a3
Jose <antares069@gmail.com>
2020-02-02 19:01:29 -0600

    objetos json

---
+++ typescript/js/clase json.js
@@ -0,0 +1,11 @@
+var bici = {
+    color: "rojo",
+    marca: "BMW",
+    frenos: "Disco",
+    velocidad_maxima: "100 km/h",
+    cambiaColor (nuevo_color){
+        this.color = nuevo_color;
+    }
+};
+
+console.log(bici);
