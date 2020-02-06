029e369f08a533d4f6a4d94d9b73e9e33817333a
Jose <antares069@gmail.com>
2020-02-05 20:26:51 -0600

    POO en TS

--- typescript/js/compilado.js
+++ typescript/js/compilado.js
@@ -1,7 +1,47 @@
 "use strict";
-var cadena = "Hola mundo";
+/*
+let cadena: string | number= "Hola mundo";
 // let cualquiera: any = "";
-var lenguas = ['PHP', 'Java', 'Javascript', 'C#'];
-var años = [12, 4, 6, 7, 89];
+let lenguas: Array <string> = ['PHP','Java', 'Javascript', 'C#'];
+// type letrasonumeros = string |number;
+let años: number[] = [12,4,6,7,89];
+
 console.log(lenguas);
 console.log(años);
+
+function getNumber(numero: number):string{
+    return "El numero es "+ numero;
+};
+
+console.log(getNumber(5));
+
+//asignanco valores al objeto cuando es publico
+camiseta.color = "rojo";
+camiseta.marca = "Nike";
+camiseta.modelo = "T-shirt";
+camiseta.talla = "L";
+camiseta.precio = 120;
+
+*/
+//clase: molde del objeto
+//propiedades: caracteristicas del objeto
+//metodos: funciones o acciones del objeto
+var Camiseta = /** @class */ (function () {
+    function Camiseta() {
+        this.color = "";
+        this.marca = "";
+        this.modelo = "";
+        this.talla = "";
+        this.precio = 0;
+    }
+    Camiseta.prototype.setColor = function (color) {
+        this.color = color;
+    };
+    Camiseta.prototype.getColor = function () {
+        return this.color;
+    };
+    return Camiseta;
+}());
+var playera = new Camiseta();
+playera.setColor("Verde");
+console.log(playera.getColor());
--- typescript/js/inicio.ts
+++ typescript/js/inicio.ts
@@ -1,8 +1,50 @@
-let cadena: string = "Hola mundo";
+/*
+let cadena: string | number= "Hola mundo";
 // let cualquiera: any = "";
 let lenguas: Array <string> = ['PHP','Java', 'Javascript', 'C#'];
-
+// type letrasonumeros = string |number;
 let años: number[] = [12,4,6,7,89];
 
 console.log(lenguas); 
 console.log(años);
+
+function getNumber(numero: number):string{
+    return "El numero es "+ numero;
+};
+
+console.log(getNumber(5));
+
+//asignanco valores al objeto cuando es publico
+camiseta.color = "rojo";
+camiseta.marca = "Nike";
+camiseta.modelo = "T-shirt";
+camiseta.talla = "L";
+camiseta.precio = 120;
+
+*/
+
+//clase: molde del objeto
+//propiedades: caracteristicas del objeto
+//metodos: funciones o acciones del objeto
+
+class Camiseta{
+    private color: string = "";
+    private marca: string = "";
+    private modelo: string = "";
+    private talla: string = "";
+    private precio: number = 0;
+
+    public setColor(color:string) {
+        this.color = color;
+    }
+
+    public getColor() {
+        return this.color;
+    }
+}
+
+let playera = new Camiseta();
+
+playera.setColor("Verde");
+
+console.log(playera.getColor());
