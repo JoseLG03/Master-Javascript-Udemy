9399d2a4f5cd370d526d7a4009bdbea159a49a5d
Jose <antares069@gmail.com>
2020-02-09 18:20:18 -0600

    importar y exportar en Angular

--- appAngular/src/app/app.component.ts
+++ appAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {configuracion} from './models/configuracion';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = 'Mi primera app con Angular';
-  
+  public title: string;
+  public descripcion: string;
   public mostrarVideojuegos: boolean = true;
+  public config:any;
+
+  constructor(){
+    this.config = configuracion;
+    this.title = configuracion.titulo;
+    this.descripcion = configuracion.descripcion;
+  }
 
   ocultarVideojuegos(value){
     this.mostrarVideojuegos = value;
---
+++ appAngular/src/app/models/configuracion.ts
@@ -0,0 +1,6 @@
+export var configuracion = {
+    color: "#eee",
+    fondo: "blue",
+    titulo: "Aplicación de Angular",
+    descripcion: "Aprendiendo Angular un domingo por la tarde"
+};
