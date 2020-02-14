229a62d134d50ed0ba734e468c2a132da6ad3dea
Jose <antares069@gmail.com>
2020-02-14 08:10:44 -0600

    if y else

--- appAngular/src/app/home/home.component.ts
+++ appAngular/src/app/home/home.component.ts
@@ -4,6 +4,16 @@ import {Component} from '@angular/core';
     selector: 'home',
     templateUrl: './home.component.html'
 })
+
 export class HomeComponent{
+    public identificado: boolean = false;
     constructor(){};
+
+    setIdentificarse(){
+        this.identificado = true;
+    }
+
+    setNoIdentificarse(){
+        this.identificado = false;
+    }
 };
