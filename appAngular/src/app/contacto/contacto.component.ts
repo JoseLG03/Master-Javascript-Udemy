4f5b802b1f96435fef3cd4dcc2b9e46ea24a8707
Jose <antares069@gmail.com>
2020-02-18 13:43:39 -0600

    form

--- appAngular/src/app/contacto/contacto.component.ts
+++ appAngular/src/app/contacto/contacto.component.ts
@@ -8,16 +8,16 @@ import { ContactoUsuario } from '../models/contacto.usuario';
 })
 export class ContactoComponent implements OnInit {
 
-  public usuario: ContactoUsuario;
+  public contacto_usuario: ContactoUsuario;
 
   constructor() {
-    this.usuario = new ContactoUsuario('','','','');
+    this.contacto_usuario = new ContactoUsuario('','','','');
   }
 
   ngOnInit(): void {
   }
 
   enviarForm(){
-    console.log("Formulario enviado");
+    console.log(this.contacto_usuario);
   }
 }
