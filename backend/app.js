70b7a5199ebdc133c16196b55a53dc69ea1003ce
Jose <antares069@gmail.com>
2020-02-26 18:04:30 -0600

    cors corregido

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@
 
 let express= require('express');
 let bodyParser = require('body-parser');
-
+var cors = require('cors');
 let app = express();
 
 //archivos de rutas
@@ -16,11 +16,11 @@ app.use(bodyParser.json());
 //cors
 // Configurar cabeceras y cors
 // configurar cabeceras http
+app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
- 
     next();
 });
 
