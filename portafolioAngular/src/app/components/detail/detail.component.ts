159d101c7d58990df961a18b926e09ca0d026c21
Jose <antares069@gmail.com>
2020-03-02 18:31:18 -0600

    traer un objeto completo del backend

--- portafolioAngular/src/app/components/detail/detail.component.ts
+++ portafolioAngular/src/app/components/detail/detail.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import {ProjectService} from '../../services/project.service';
+import {Global} from '../../services/global';
+import {Router, ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
+  providers: [ProjectService]
 })
+
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  public url:string;
+  public project : Project;
+
+  constructor(
+    private _projectService : ProjectService,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
+    this.url = Global.url;
+   }
 
   ngOnInit(): void {
+    this._route.params.subscribe(params =>{
+      let id = params.id;
+
+      this.getProjectDetail(id);
+    })
   }
 
+  getProjectDetail(id){
+    this._projectService.getProjectDetail(id).subscribe(
+      response =>{
+        this.project = response.project;
+      },
+      error =>{
+        console.log(<any>error);
+      }
+    )
+  }
 }
