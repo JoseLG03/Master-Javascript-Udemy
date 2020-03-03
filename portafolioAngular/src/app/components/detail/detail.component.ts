728d4bab41793ca669c6f1f8e8faafdeb86c6608
Jose <antares069@gmail.com>
2020-03-02 20:22:00 -0600

    borrar proyecto

--- portafolioAngular/src/app/components/detail/detail.component.ts
+++ portafolioAngular/src/app/components/detail/detail.component.ts
@@ -42,4 +42,17 @@ export class DetailComponent implements OnInit {
       }
     )
   }
+
+  deleteProject(id){
+    this._projectService.deleteProject(id).subscribe(
+      response =>{
+        if(response.project){
+          this._router.navigate(['/proyectos']);
+        }
+      },
+      error =>{
+        console.log(<any>error);
+      }
+    );
+  }
 }
--- portafolioAngular/src/app/services/project.service.ts
+++ portafolioAngular/src/app/services/project.service.ts
@@ -33,4 +33,8 @@ export class ProjectService{
         let headers = new HttpHeaders().set('Content-Type','application/json');
         return this._http.get(this.url+'project/'+id, {headers:headers});
     }
+    deleteProject(id): Observable<any>{
+        let headers = new HttpHeaders().set('Content-Type','application/json');
+        return this._http.delete(this.url+'project/'+id, {headers:headers});
+    }
 }
