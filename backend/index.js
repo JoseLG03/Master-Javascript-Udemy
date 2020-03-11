37ca90d7536bfbff624d26146deaebada399aad8
Jose <antares069@gmail.com>
2020-03-10 19:53:53 -0600

    preparando para subir proyecto

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,13 @@ let port = 3700;
 mongus.set('useFindAndModify', false);
 mongus.Promise = global.Promise;
 
-mongus.connect('mongodb://localhost:27017/portafolio', { useNewUrlParser: true , useUnifiedTopology: true})
+mongus.connect('mongodb+srv://auratus_03:<auratus_03>@cluster0-azdyr.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true , useUnifiedTopology: true})
       .then(()=>{
           console.log("Conexión exitosa");
           
           //creación del servidor
-          app.listen(port, ()=>{
-              console.log("Servidor arriba");
-          })
+          //app.listen(port, ()=>{
+          //    console.log("Servidor arriba");
+          //})
       })
       .catch(error=>console.log(error));  
